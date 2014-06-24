4b7332d48f0bc3a3b69dce1047f832f972ab37f9
Bryan Warner <bwarner@traackr.com>
2014-06-24 14:13:27 -0400

    Making collection name configurable

--- src/main/java/com/traackr/influencer/persistance/ExampleMongoDbRepository.java
+++ src/main/java/com/traackr/influencer/persistance/ExampleMongoDbRepository.java
@@ -3,6 +3,7 @@ package com.traackr.influencer.persistance;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.DB;
@@ -16,9 +17,12 @@ public class ExampleMongoDbRepository {
   
   private DBCollection collection;
   
+  @Value("${mongodb.collection.example}")
+  private String       collectionName;
+  
   @PostConstruct
   public void init() {
-    this.collection = traackrDb.getCollection("test");
+    this.collection = traackrDb.getCollection(collectionName);
   }
   
   public DBCollection getCollection() {
